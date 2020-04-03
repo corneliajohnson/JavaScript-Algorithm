513c2e91881ae34e1b135ad4117850ec3b65dc34
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2020-04-02 20:24:36 -0400

    challenge one - add two numbers

---
+++ challenge1.js
@@ -0,0 +1,19 @@
+
+//Challenge1: Add Two Number
+
+//my solution
+addTwoNumbers = (num1, num2) => (num1 + num2);
+
+console.log(addTwoNumbers(5, 6));
+console.log(addTwoNumbers(10, 0));
+
+
+//instructor solution
+
+function addTwoNumbers(num1, num2)
+{
+  return num1 + num2;
+}
+
+console.log(addTwoNumbers(5, 6));
+console.log(addTwoNumbers(10, 0));
