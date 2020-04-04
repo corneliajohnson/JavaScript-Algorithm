07dbc712d141cd2d04ede66f86a0e57e9da120e7
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2020-04-03 20:08:52 -0400

    added challenge 3

---
+++ challenge3.js
@@ -0,0 +1,15 @@
+//Write a function that converts Celsius to Fahrenheit
+
+
+//My solution
+function toFahrenheit(celsius){
+  return celsius * 9 / 5 + 32;
+}
+
+console.log(toFahrenheit(-30))
+
+
+
+
+
+//Instructor Solution - Same as mine
